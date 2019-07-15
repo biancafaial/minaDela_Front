ed72799ffde204d951e2fde1659860450d27341c
biancafaial <biancafaial@gmail.com>
2019-07-14 22:53:30 -0300

    Categorias no index e Fitro de busca de categorias funcinando

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,28 +1,27 @@
-const botao = document.querySelector('#criar_mina_button')
-botao.addEventListener("click", criarMina)
+  const botao = document.querySelector('#criar_mina_button')
+  botao.addEventListener("click", criarMina)
 
-function criarMina () {
-  const nome = document.querySelector("#nome_input").value
-  const telefone = document.querySelector("#telefone_input").value
-  const email = document.querySelector("#email_input").value
-  const cpf = document.querySelector("#cpf_input").value
+  function criarMina () {
+    
+    const nome = document.querySelector("#nome_input").value
+    const telefone = document.querySelector("#telefone_input").value
+    const email = document.querySelector("#email_input").value
+    const cpf = document.querySelector("#cpf_input").value
+   
 
-  const categoria = document.querySelector("#nome_input").value
- 
-
-  const descricao = document.querySelector("#descricao_input").value
-  const senha = document.querySelector("#senha_input").value
-  const mina = {
-    nome, telefone, email, cpf, categoria, descricao, senha
-  }
-  fetch(
-    'http://localhost:3000/minas',
-    {
-      method: 'POST',
-      body: JSON.stringify(mina),
-      headers: {
-        'Content-Type': 'application/json'
-      }
+    const categoria = document.querySelector("input[type='radio']:checked").value
+    const descricao = document.querySelector("#descricao_input").value
+    const mina = {
+      nome, telefone, email, cpf, categoria, descricao
     }
-  ).then(response => console.log("criou!"))
-  }
+    fetch(
+      'http://localhost:3000/minas',
+      {
+        method: 'POST',
+        body: JSON.stringify(mina),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(response => console.log("criou!"))
+    }
---
+++ js/perfis.js
@@ -0,0 +1,41 @@
+var urlParams = new URLSearchParams(window.location.search);
+let categoria = urlParams.get("categoria")
+const boxPerfil = document.querySelector('.maravilhosas__box');
+
+
+
+fetch("http://localhost:3000/minas/" + categoria).then(response => response.json() )
+    .then((minas) => {
+
+        minas.forEach((mulher) => {
+            const card = document.createElement('div');
+            card.setAttribute('class', 'maravilhosas__perfil');
+            boxPerfil.appendChild(card);
+
+            // definir imagem
+            const img = document.createElement('img');
+            img.setAttribute('class', 'img-responsive');
+
+            //comparação caso a imagem seja undefined o else irá definir o png como imagem do card
+            // outro mode de fazer a comparação usando OU if (mulher.metadata == undefined || mulher.metadata.image == "")
+            if (mulher && mulher.imagem) {
+                img.setAttribute('src', mulher.imagem);
+            } else {
+                img.setAttribute('src', './img/img-mulher.png');
+            }
+            card.appendChild(img);
+
+            const nome = document.createElement('p');
+            nome.textContent = mulher.nome;
+            card.appendChild(nome);
+
+            const categoria = document.createElement('p');
+            categoria.textContent = mulher.categoria;
+            card.appendChild(categoria);
+
+            const descricao = document.createElement('p');
+            descricao.textContent = mulher.descricao;
+            card.appendChild(descricao);
+
+        })
+    })
